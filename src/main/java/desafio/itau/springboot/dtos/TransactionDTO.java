0c7c193e8c44856a0d600f1f7b7f26a4a0457308
nicolasvaranda <nicolasbro@hotmail.com>
2025-03-13 00:20:29 -0300

    feat: release challenge v2

--- src/main/java/desafio/itau/springboot/dtos/TransactionDTO.java
+++ src/main/java/desafio/itau/springboot/dtos/TransactionDTO.java
@@ -1,13 +1,11 @@
 package desafio.itau.springboot.dtos;
 
-import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotNull;
 
 import java.time.OffsetDateTime;
 
 public class TransactionDTO {
     @NotNull
-    @Min(0)
     private Double valor;
     @NotNull
     private OffsetDateTime dataHora;
--- src/main/java/desafio/itau/springboot/services/TransactionService.java
+++ src/main/java/desafio/itau/springboot/services/TransactionService.java
@@ -3,6 +3,8 @@ package desafio.itau.springboot.services;
 import desafio.itau.springboot.models.Transaction;
 import org.springframework.stereotype.Service;
 
+import java.time.OffsetDateTime;
+import java.util.DoubleSummaryStatistics;
 import java.util.Queue;
 import java.util.concurrent.ConcurrentLinkedDeque;
 
@@ -10,7 +12,21 @@ import java.util.concurrent.ConcurrentLinkedDeque;
 public class TransactionService {
 
     private final Queue<Transaction> transactions = new ConcurrentLinkedDeque<>();
-    // TODO
-    // addTransaction
+
+    public void addTransaction(Transaction transaction) {
+        transactions.add(transaction);
+    }
+
+    public void clearTransactions() {
+        transactions.clear();
+    }
+
+    public DoubleSummaryStatistics getStats () {
+        OffsetDateTime now = OffsetDateTime.now();
+        return transactions.stream()
+                .filter(t -> t.getDataHora().isAfter(now.minusSeconds(60)))
+                .mapToDouble(Transaction::getValor)
+                .summaryStatistics();
+    }
 
 }
